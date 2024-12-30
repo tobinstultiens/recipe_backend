5f9608b35083a1a89ff1b511327fcb74085ccc5f
Tobin Stultiens <tobin.stultiens@gmail.com>
2024-12-30 23:05:22 +0100

    Updated rule

--- lib/ingredient.ml
+++ lib/ingredient.ml
@@ -1,12 +1,5 @@
 type metric = Gram of int | Mililiter of int | KiloGram of int
-
-type imperial =
-  | Teaspoon of int
-  | Tablespoon of int
-  | Ounce of int
-  | FluidOunce of int
-  | Unit of int
-
+type imperial = Teaspoon of int | Tablespoon of int | Ounce of int | FluidOunce of int | Unit of int
 type amount_of_ingredient = Metric of metric | Imperial of imperial
 type ingredient = { name : string; amount : amount_of_ingredient }
 
@@ -22,8 +15,5 @@ let to_string_metric = function
   | Mililiter v -> Printf.sprintf "%d" v ^ " ml"
   | KiloGram v -> Printf.sprintf "%d" v ^ " kg"
 
-let to_string = function
-  | Metric m -> to_string_metric m
-  | Imperial i -> to_string_imperial i
-
+let to_string = function Metric m -> to_string_metric m | Imperial i -> to_string_imperial i
 let make name ingredient = Some { name; amount = ingredient }
